277f228dfaca8785edb2af70bbb430d51e4a272b
desolatecoder <sairakesh31808@gmail.com>
2023-06-09 16:06:15 +0530

    ternary operator with template literals

--- html/js/starter/evenarray.js
+++ html/js/starter/evenarray.js
@@ -1,7 +1,7 @@
 let b=[1,2,3,4,5]
 for (let i = 0; i < b.length; i++) {
-var evenNumb=b[i]%2==0?`${b[i]} i'm even`:b[i]+"i'm odd"
-console.log(evenNumb);
+var evenNumbs=b[i]%2==0?`${b[i]} i'm even`:b[i]+"i'm odd"
+console.log(evenNumbs);
 
     if (b[i]%2==0) {
         // var temp=`${b[i]==2?'im two':"i'm not 2"}`
